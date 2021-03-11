306a9a543a07f29d74e742f2402cd66fa9c4b287
Lars Brinkhoff <lars@nocrew.org>
2021-03-11 13:27:49 +0000

    Files extracted from 169268.tape

---
+++ novafield/plymac.h
@@ -0,0 +1,160 @@
+
+(CNFE:2)PLYMACH
+CURRENT AS OF: 13 JANUARY 1986
+
+                         NETWORK FIELD ENGINEERING
+                         *************************
+
+                        MICHIGAN DISTRICT
+
+                         AREAS OF RESPONSIBILITY
+                         ------------------------
+
+A.  DISTRICT MANAGER        BILL NAVAS          OFFICE:  313/855-0044
+
+B.  REGIONAL TECH SUPPORT   HERVEY WILLIS       OFFICE: 214/637-7416
+
+------------------------------------------------------------------------------
+
+
+
+1.  LARRY MACEK         OFFICE: 313/453-7640   (FSC.L/MACEK)
+      9 MACHINES
+     ----------
+
+      CITY             NODE NAME/#        TYPE OF MACH.      LOCATION
+---------------        -----------        -------------      ---------------
+
+PLYMOUTH, MI.          ND4533/4533        ENG./ISIS          TYMNET, INC.
+PLYMOUTH, MI.          ND4534/4534        ENG./SWITCHER      TYMNET, INC.
+PLYMOUTH, MI.          ND4535/4535        ENG./SWITCHER      TYMNET, INC.
+PLYMOUTH, MI.          ND4536/4536        ENG./SWITCHER      TYMNET, INC.
+PLYMOUTH, MI.          ND4541/4541        ENG./ISIS          TYMNET, INC.
+PLYMOUTH, MI.          ND4542/4542        ENG./TYMSAT        TYMNET, INC.
+PLYMOUTH, MI.          ND4543/4543        ENG./ISIS          TYMNET, INC.
+PLYMOUTH, MI.          ND4544/4544        ENG./ISIS          TYMNET, INC.
+PLYMOUTH, MI.          ND4545/4545        ENG./ISIS          TYMNET, INC.
+
+
+2.  KEN LUMM           OFFICE:  313/453-7640   (FSC.K/LUMM)
+     21 MACHINES
+     ----------
+
+      CITY             NODE NAME/#        TYPE OF MACH.      LOCATION
+---------------        -----------        -------------      ----------------
+
+PLYMOUTH, MI.          ND2360/2360        ENG./TYMSAT        TYMSHARE
+ROSEVILLE, MI.         ND5046/5046        ENG./MICRO         MCCULLOUGH LEASING
+ANN ARBOR, MI.         ND2165/2165        ENG./TYMSAT    DP  M.D.S.I.
+ANN ARBOR, MI.         ND3735/3735        ENG./MICRO         TYMNET, INC.
+ANN ARBOR, MI.         DI2120/2120        ENG./MICRO         TYMNET (DIALOG)
+ANN ARBOR, MI.         VA2054/2054        ENG./MINI          VADATS
+DEARBORN, MI.          ND3616/3616        ENG./ISIS          FORD MOTOR CO.
+DEARBORN, MI.          ND3244/3244        ENG./ISIS          FORD MOTOR CO.
+DEARBORN, MI.          ND6755/6755        ENG./ISIS          FORD MOTOR CO.
+DEARBORN, MI.          ND7175/7175        ENG./ISIS          FORD MOTOR CO.
+JACKSON, MI.           ND2343/2343        ENG./TYMSAT        TYMNET/CONSUMER PR
+JACKSON, MI.           ND3344/3344        ENG./MICRO         TYMNET/CONSUMER PR
+TRAVERSE CITY, MI.     ND5063/5063        ENG./MICRO         TYMNET, INC.
+ROYAL OAK, MI.         ND2245/2245        ENG./MICRO         TYMNET, INC.
+BENTON HARBOR, MI.     ND4225/4225        ENG./MICRO         TYMNET, INC.
+FREELAND, MI.          ND4467/4467        ENG./TYMSAT        TYMNET, INC.
+IRON MOUNTAIN, MI.     VA2205/2205        ENG./MINI          VADATS
+PLYMOUTH, MI.          ND4532/4532        ENG./SWITCHER      TYMNET, INC.
+PLYMOUTH, MI.          ND4537/4537        ENG./SWITCHER      TYMNET, INC.
+PLYMOUTH, MI.          ND4540/4540        ENG./SWITCHER      TYMNET, INC.
+PLYMOUTH, MI.          ND3164/3164        ENG./LAB           TYMNET, INC.
+
+
+3.  GLENN WILKOS       OFFICE:  313/453-7640    (FSC.G/WILKOS)
+     19 MACHINES
+     -----------
+
+        CITY           NODE NAME/#        TYPE OF MACH.      LOCATION
+---------------        ------------       -------------      ---------------
+
+PLYMOUTH, MI.          ND4546/4546        ENG./ISIS          TYMNET, INC.
+PLYMOUTH, MI.          ND4547/4547        ENG./ISIS          TYMNET, INC.
+PLYMOUTH, MI.          ND4550/4550        ENG./ISIS          TYMNET, INC.
+GRAND RAPIDS, MI.      ND4117/4117        ENG./MICRO         TYMNET, INC.
+LANSING, MI.           ND3666/3666        ENG./MICRO         TYMNET, INC.
+LANSING, MI.           ND2272/2272        ENG./MICRO         STS CONSULTANTS
+STERLING HTS., IL.     ND6402/6402        ENG./MICRO         CONTEMPO CASUALS
+SOUTHFIELD, MI.        ND3670/3670        ENG./MICRO         TYMNET, INC.
+SOUTHFIELD, MI.        ND3654/3654        ENG./MICRO         TYMNET, INC.
+SOUTHFIELD, MI.        TRW1040/1040       ENG./TYMSAT  DP    TRW
+DETROIT, MI.           ND2225/2225        ENG./ISIS          TYMNET, INC.
+DETROIT, MI.           ND3530/3530        ENG./ISIS          TYMNET, INC.
+DETROIT, MI.           ND4501/4501        ENG./ISIS    DP    TYMNET, INC.
+DETROIT, MI.           ND6542/6542        ENG./DMINI         TYMNET, INC.
+DETROIT, MI.           ND6543/6543        ENG./DMINI         TYMNET, INC.
+DETROIT, MI.           DI2117/2117        ENG./MICRO         TYMNET (DIALOG)
+DETROIT, MI.           VA2170/2170        ENG./MICRO         VADATS
+TROY, MI.              DU22246/2246       ENG./ISIS          DUPONT
+TROY, MI.              DU2263/2263        ENG./MICRO         DUPONT
+
+
+4.  TIM ZALESKI        OFFICE:   313/453-7640   (FSC.T/ZALESKI)
+     20 MACHINES
+     -----------
+
+
+        CITY           NODE NAME/#       TYPE OF MACH.       LOCATION
+----------------       -----------       --------------      ----------------
+
+PLYMOUTH, MI.          ND5064/5064       ENG./MICRO          TYMNET
+PLYMOUTH, MI.          ND5065/5065       ENG./MICRO          TYMNET
+PLYMOUTH, MI.          ND5066/5066       ENG./MICRO          TYMNET
+PLYMOUTH, MI.          ND6474/6474       ENG./DMINI          TYMNET
+PLYMOUTH, MI.          ND6475/6475       ENG./DMINI          TYMNET
+PLYMOUTH, MI.          ND6476/6476       ENG./DMINI          TYMNET
+PLYMOUTH, MI.          ND6477/6477       ENG./DMINI          TYMNET
+PLYMOUTH, MI.          ND5471/5471       ENG./MICRO          TYMNET
+ALLEN PARK, MI.        VA2114/2114       ENG./MINI           VADATS
+KALAMAZOO, MI.         DI2121/2121       ENG./MICRO          TYMNET (DIALOG)
+KALAMAZOO, MI.         ND3516/3516       ENG./ISIS           TYMNET, INC.
+KALAMAZOO, MI.         ND4134/4134       ENG./ISIS           TYMNET, INC.
+KALAMAZOO, MI.         ND4400/4400       ENG./ISIS           TYMNET, INC.
+YPSILANTI, MI.         ND6427/6427       ENG./ISIS           FORD MOTOR CO.
+YPSILANTI, MI.         ND6430/6430       ENG./MICRO          FORD MOTOR CO.
+PLYMOUTH, MI.          ND4531/4531       ENG./SWITCHER       TYMNET, INC.
+TOLEDO, OH.            ND3566/3566       ENG./MICRO          TYMNET, INC.
+FOSTORIA, OH.          ND6446/6446       ENG./ATC            BOC GROUP
+LIMA, OH.              ND2376/2376       ENG/MICRO           TYMNET, INC.
+LIMA, OH.              ND2520/2520       ENG./ATC            BOC GROUP
+
+
+5.  MITCH SOTAK        OFFICE:   216/524-3913   (FSC.M/SOTAK)
+     22 MACHINES
+
+       CITY            NODE NAME/#        TYP OF MACH.      LOCATION
+----------------       -----------        -------------     --------------
+
+CLEVELAND, OH.         ND4063/4063        ENG./ISIS         TYMNET, INC.
+CLEVELAND, OH.         ND4530/4530        ENG./ISIS         TYMNET, INC.
+CLEVELAND, OH.         ND4552/4552        ENG./ISIS         TYMNET, INC.
+CLEVELAND, OH.         ND4671/4671        ENG./ISIS         TYMNET, INC.
+CLEVELAND, OH.         ND5317/5317        ENG./ISIS         TYMNET, INC.
+CLEVELAND, OH.         ND5320/5320        ENG./ISIS         TYMNET, INC.
+CLEVELAND, OH.         ND3441/3441        ENG./SWITCHER     TYMNET, INC.
+CLEVELAND, OH.         ND4647/4647        ENG./ISIS         TYMNET, INC.
+CLEVELAND, OH.         ND6222/6222        ENG./ISIS         TYMNET, INC.
+CLEVELAND, OH.         ND6223/6223        ENG./ISIS         TYMNET, INC.
+CLEVELAND, OH.         ND6677/6677        ENG./MICRO        TELECHECK
+CLEVELAND, OH.         ND5115/5115        ENG./ISIS         SOHIO
+CLEVELAND, OH.         DI2115/2115        ENG./MICRO        TYMNET (DIALOG)
+CLEVELAND, OH.         VA2130/2130        ENG./MICRO        VADATS
+SANDUSKY, OH.          ND6431/6431        ENG./MICRO        FORD MOTOR CO.
+BREAKSVILLE, OH.       VA2117/2117        ENG./MINI         VADATS
+MAYFIELD, OH.          ND2574/2574        ENG./ATC          NETECH
+AKRON, OH.             ND3111/3111        ENG./TSAT         CITYWIDE
+EASTLAKE, OH.          ND3471/3471        ENG./ISIS         EATON CORP.
+MENTOR, OH.            ND3551/3551        ENG./MICRO        MISCO 
+YOUNGSTOWN, OH.        ND4222/4222        ENG./MICRO        TYMNET, INC.
+WARREN, OH.            ND7205/7205        ENG./ATC          BOC / AIRCO
+
+
+
+
+
+   
