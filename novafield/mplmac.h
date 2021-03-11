f11d3993578ee436f52474a52679e07156351452
Lars Brinkhoff <lars@nocrew.org>
2021-03-11 13:10:20 +0000

    Files extracted from 169268.tape

---
+++ novafield/mplmac.h
@@ -0,0 +1,153 @@
+CURRENT AS OF 13 JUNE 1986                               (CNFE:2)MPLMACH
+                                                        (CNFE:55)MPLMACH.ONE
+
+                                                        
+                                                        
+                   NETWORK FIELD ENGINEERING            
+                   =========================            
+                                                        
+                   MINNEAPOLIS DISTRICT                 
+                                                        
+                   AREAS OF RESPONSIBILITY              
+                   --------------------------
+                   
+                                                        
+A.  MANAGER   KEVIN OLINGER (FSC.K/OLINGER)        OFFICE:  612/938-6603   
+                                                        
+B.  TECH SUPPORT    HERVEY WILLIS                  OFFICE:  214/637-7416   
+                                                        
+--------------------------------------------------------------------------------DISTRICT TOTAL  102 MACHINES
+
+                                                        
+                                                        
+JACK CLARK  24 MACHINES                                 OFFICE:  612/333-4354  
+(FSC.J/CLARK)
+                                                        
+CITY               NODE                TYPE             LOCATION
+                                                        
+MINNEAPOLIS, MN    ND2036              ISIS             TYMNET
+MINNEAPOLIS, MN    ND2037              ISIS             TYMNET
+MINNEAPOLIS, MN    ND2063              ISIS             TYMNET
+LACROSSE, WI       ND2720              MICRO            TYMNET
+FARGO, ND          ND4751              MICRO            TYMNET
+SOUIX FALLS, SD    ND4752              MICRO            TYMNET
+RAPID CITY, SD     ND5013              MICRO            TYMNET
+EAU CLAIRE, WI     ND5362              MICRO            TYMNET
+MINNEAPOLIS, MN    ND5730              DMINI            U.S. POSTAL SER.
+MINNEAPOLIS, MN    ND5731              DMINI            U.S. POSTAL SER.
+MINNEAPOLIS, MN    ND6243              MINI             TYMNET
+BISMARCK, ND       ND7124              MICRO            TYMNET
+GRAND FORKS, ND    ND7340              MICRO            TYMNET
+MINNEAPOLIS, MN    ND7426              MICRO            3M
+MINOT, ND          ND7457              MICRO            TYMNET
+SOUIX FALLS, SD    ND7601              MINI             NETECH
+MINNEAPOLIS, MN    VA2122              MINI             VADATS
+ST. PAUL, MN       VA2157              MINI             VADATS
+FARGO, ND          VA2200              MINI             VADATS
+SOUIX FALLS, SD    VA2232              MINI             VADATS
+SOUIX FALLS, SD    VA2265              MINI             VADATS
+FT. MEADE, SD      VA2353              MINI             VADATS
+HOT SPRINGS, SD    VA2356              MINI             VADATS
+FARGO, ND          VA2406              MINI             VADATS
+                                                        
+                                                        
+                                                        
+BILL DECKER   27 MACHINES                               OFFICE:  612/333-4354  
+(FSC.W/DECKER)     
+                                                        
+CITY               NODE                TYPE             LOCATION
+                                                        
+MINNEAPOLIS, MN    DI2122              MINI             DIALOG/TYMNET
+MINNEAPOLIS, MN    ND2370              ISIS             TYMNET
+MINNEAPOLIS, MN    ND2377              ISIS             TYMNET
+MINNEAPOLIS, MN    ND2406              ISIS             TYMNET
+MINNEAPOLIS, MN    ND3445              ISIS             HONEYWELL
+MINNEAPOLIS, MN    ND3446              ISIS             HONEYWELL
+MINNEAPOLIS, MN    ND3571              ISIS             TYMNET
+MINNEAPOLIS, MN    ND4255              ISIS             HONEYWELL
+MINNEAPOLIS, MN    ND4316              ISIS             TYMNET
+MINNEAPOLIS, MN    ND4317              ISIS             TYMNET
+MINNEAPOLIS, MN    ND4320              ISIS             TYMNET
+MINNEAPOLIS, MN    ND4321              ISIS             TYMNET
+MINNEAPOLIS, MN    ND4322              ISIS             TYMNET
+MINNEAPOLIS, MN    ND4341              MINI             REPUBLIC TELECOM
+MINNEAPOLIS, MN    ND4424              ISIS             TYMSHARE (TTMS)
+MINNEAPOLIS, MN    ND4425              ISIS             TYMSHARE (TTMS)
+MINNEAPOLIS, MN    ND5023              MICRO            HONEYWELL
+MINNEAPOLIS, MN    ND5145              LAB              TYMNET
+MINNEAPOLIS, MN    ND5152              ISIS             TYMNET
+MINNEAPOLIS, MN    ND5153              ISIS             TYMNET
+MINNEAPOLIS, MN    ND6244              MICRO            HONEYWELL
+ST. PAUL, MN       WP10242             ATC/SAT          WEST PUBLISHING
+ST. PAUL, MN       WP10243             ATC/SAT          WEST PUBLISHING
+MINNEAPOLIS, MN    ND7030              MICRO            HONEYWELL
+ST. PAUL, MN       ND7100              MICRO            BLUE CROSS/SHIELD
+EDEN, PRARIE, MN   ND7556              MICRO            MCGRAW HILL
+ST CLOUD, MN       VA2363              MINI             VADATS
+                                                        
+                                                        
+JOHN MAEGLI    31 MACHINES                              OFFICE:  612/333-4354  
+(FSC.J/MAEGLI)     
+                                                        
+CITY               NODE                TYPE             LOCATION
+                                                        
+RACINE             VA10003             MIN.(DUAL)       S. C. JOHNSON
+RACINE             VA10004             MIN.(DUAL)       S. C. JOHNSON
+WAUWATOSA          VA10222             ENG/ISIS         SUNDSTRAND
+MADISON            ND2115              MIN.             VADATS
+WOOD               ND2172              MIN.(PSM)        VADATS
+WOOD               ND2241              MIN.(PSM)        VADATS
+BROOKFIELD         ND2403              ENG/ISIS         TYMNET
+BROOKFIELD         ND2407              ENG/ISIS         TYMNET
+BROOKFIELD         ND2440              ENG/TSAT         TYMNET
+BROOKFIELD         ND2603              ENG/ISIS         TYMNET
+MADISON            ND3066              ENG/TSAT         DYNATAX
+BROOKFIELD         ND3075              ENG/ISIS         TYMNET
+W.MILWAUKEE        ND3646              ENG/ISIS         REXNORD
+GREEN BAY          ND3651              MICRO            TYMNET
+NEENAH             ND4205              MICRO            TYMNET
+WEST BEND          ND4476              MICRO            TYMNET
+BROOKFIELD         ND4566              ENG/TSAT         TYMNET
+BROOKFIELD         ND4567              ENG/ISIS         TYMNET
+BROOKFIELD         ND4703              ENG/ISIS         TYMNET
+OSHKOSH            ND4770              MICRO            TYMNET
+BROOKFIELD         ND5025              ENG/SWIT         TYMNET
+BROOKFIELD         ND5031              ENG/SWIT         TYMNET
+BROOKFIELD         ND5033              ENG/ISIS         TYMNET
+BROOKFIELD         ND5034              ENG/ISIS         TYMNET
+MILWAUKEE          ND5134              ENG/ISIS         NW MUTUAL LIFE
+MILWAUKEE          ND5135              ENG/ISIS         NW MUTUAL LIFE
+BROOKFIELD         ND5544              ENG/ISIS         TYMNET
+BROOKFIELD         ND5545              ENG/ISIS         TYMNET
+BROOKFIELD         ND5546              ENG/ISIS         TYMNET
+KENOSHA            ND7305              MICRO            TYMNET
+BELOIT             ND7460              MICRO            TYMNET
+                                                        
+                                                        
+JIM MUCK   20 MACHINES                                  OFFICE:  612/333-4354  
+(FSC.J/MUCK)       
+                                                        
+CITY               NODE                TYPE             LOCATION
+                                                        
+MINNEAPOLIS, MN    ND3570              ISIS             UCCPAC
+MINNEAPOLIS, MN    ND4465              MINI             TYMSHARE
+MINNEAPOLIS, MN    ND4577              MICRO            HONEYWELL
+MINNEAPOLIS, MN    ND4655              ISIS             3M
+MINNEAPOLIS, MN    ND5114              ISIS             3M
+BLOOMINGTON, MN    ND5203              MICRO            FAIRCHILD CAMERA
+BLOOMINGTON, MN    ND5204              MINI             TELECHECK
+PLYMOUTH, MN       ND5264              MICRO            NORSTAN COMM.
+ROCHESTER, MN      ND6545              SAT              TYMNET
+EAGAN, MN          ND7014              ATC/SAT          AIRCO
+MINETONKA          ND7504              MICRO            MDFSC
+MINNEAPOLIS, MN    ND7577              ISIS             NETECH
+BLOOMINGTON, MN    UC1013              MINI             CONTROL DATA
+MINNEAPOLIS, MN    WP3154              MINI             WEST PUBLISHING
+MINNEAPOLIS, MN    WP3213              MINI             WEST PUBLISHING
+ROSEVILLE, MN      ND2452              ATC/SAT          CONTEMPO CASUAL
+TOMAH, WI          VA2377              MINI             VADATS
+ST. PAUL, MN       WP10271             MICRO            WEST PUBLISHING
+ST. PAUL, MN       WP10272             MICRO            WEST PUBLISHING
+ST. PAUL, MN       WP6700              ATC/SAT          WEST PUBLISHING
+
+   
