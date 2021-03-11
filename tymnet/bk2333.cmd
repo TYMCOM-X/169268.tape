7c8fd188d97874dde2c150906b65a4f459d54843
Lars Brinkhoff <lars@nocrew.org>
2021-03-11 17:31:17 +0000

    Files extracted from 169268.tape

---
+++ tymnet/bk2333.cmd
@@ -0,0 +1,8 @@
+;; ISIS-II Version 7.03 Sample Command File
+;@(SOURCE)I2IS07.I03
+;@(tymnet)nd2333.tym
+;@(tymnet)ggisis.ltd
+;@(SOURCE)I2IS07.R03
+;X(SOURCE)I2IS07.P03
+1;Fnd2333.nib
+70;T0,SUMTBL;PINTLEN,IEND-INTLEN;P%PSEGF,SFSIZE;P%P%Q
